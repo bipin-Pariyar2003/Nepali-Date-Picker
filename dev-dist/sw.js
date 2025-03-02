45f55f7e202a9e83eedaf9a1976b3516e2d34d56
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-03-02 14:30:39 +0545

    cleared cache

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ep7o5o04s5"
+    "revision": "0.dc3bs3q9oqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

3acbbb90c1621d0ee36a2fe8bb6a0d51340c0715
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-03-09 14:38:33 +0545

    updated shrinking height of bsview

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,163 +1,6 @@
-// import { useState } from "react";
-// import moment from "moment";
-
-// import NepDatePicker from "Components/NepDatePicker";
-// import { ad2bsHandler, bs2adHandler } from "utils";
-
-// import BsView from "Views/BsView";
-// import AdView from "Views/AdView";
-
-// import EngDatePicker from "Components/EngDatePicker";
-// import { Box, Stack, Typography } from "@mui/material";
-// import ResetBtn from "Views/ResetBtn";
-
-// function Home() {
-//   const initialEnglishDate = moment().format("YYYY/MM/DD");
-//   const initialNepaliDate = ad2bsHandler(initialEnglishDate);
-
-//   // State
-//   const [selectedNepaliDate, setSelectedNepaliDate] = useState(initialNepaliDate);
-//   const [selectedEnglishDate, setSelectedEnglishDate] = useState(initialEnglishDate);
-
-//   // Handling Nepali date change
-//   const handleNepaliDateChange = (date) => {
-//     setSelectedNepaliDate(date);
-//     const engDate = bs2adHandler(date);
-//     setSelectedEnglishDate(engDate);
-//   };
-
-//   // Handling English date change
-//   const handleEngDateChange = (date) => {
-//     setSelectedEnglishDate(date);
-//     const nepDate = ad2bsHandler(date);
-//     setSelectedNepaliDate(nepDate);
-//   };
-
-//   return (
-//     <Stack
-//       sx={{
-//         height: { xs: "100svh", md: "100svh" },
-//         gap: { xs: 0, md: 2 },
-//       }}
-//     >
-//       {/* Heading */}
-//       <Box
-//         className="heading"
-//         sx={{ height: { xs: "30px", md: "50px" }, textAlign: "center" }}
-//       >
-//         <Typography
-//           sx={{
-//             fontSize: { xs: "1rem", md: "1.5rem" },
-//             fontWeight: "bold",
-//           }}
-//         >
-//           Nepali Date Converter (B.S - A.D)
-//         </Typography>
-//       </Box>
-
-//       {/* Main Container */}
-//       <Stack
-//         direction={{ xs: "column", md: "row" }}
-//         gap={{ xs: 2, md: 2 }}
-//         sx={{
-//           overflow: "hidden",
-//           width: "100%",
-//           display: "flex",
-//           flex: 1,
-//         }}
-//       >
-//         {/* B.S View */}
-//         <Stack
-//           sx={{
-//             gap: { xs: 0, md: 2 },
-//             border: "1px solid black",
-//             flex: 1,
-//             m: { xs: 1, md: 2 },
-//             mt: { xs: 0, md: 0 },
-//           }}
-//         >
-//           <Typography
-//             variant="h5"
-//             sx={{
-//               fontWeight: "bold",
-//               textAlign: "center",
-//               textDecoration: { xs: "none", md: "underline" },
-//               padding: "6px",
-//               fontSize: { xs: "1rem", md: "1.5rem" },
-//               height: { xs: "30px", md: "40px" },
-//             }}
-//           >
-//             Select date in B.S.
-//           </Typography>
-//           <BsView selectedDate={selectedNepaliDate} onChange={handleNepaliDateChange} />
-//         </Stack>
-
-//         {/* Date Pickers */}
-//         <Box
-//           sx={{
-//             display: "flex",
-//             flexDirection: "column",
-//             gap: { xs: "10px", md: "20px" },
-//             padding: { xs: "0px", md: "20px" },
-//             alignItems: "center",
-//             justifyContent: "center",
-//             flex: 1,
-//             width: { xs: "100%", md: "350px" }, // Mobile: Full width, Desktop: Fixed width
-//             margin: "0 auto",
-//           }}
-//         >
-//           <NepDatePicker
-//             selectedDate={selectedNepaliDate}
-//             onChange={handleNepaliDateChange}
-//             sx={{ width: { xs: "80%", sm: "100%" } }} // Mobile: 80%, Desktop: 100%
-//           />
-//           <EngDatePicker
-//             sx={{ height: "min-content", width: { xs: "80%", sm: "100%" } }} // Mobile: 80%, Desktop: 100%
-//             selectedDate={selectedEnglishDate}
-//             onChange={handleEngDateChange}
-//           />
-//           <ResetBtn
-//             setSelectedEnglishDate={setSelectedEnglishDate}
-//             onChange={handleEngDateChange}
-//           />
-//         </Box>
-
-//         {/* A.D View */}
-//         <Stack
-//           sx={{
-//             gap: { xs: 0, md: 2 },
-//             border: "1px solid black",
-//             flex: 1,
-//             m: { xs: 1, md: 2 },
-//             mt: { xs: 0, md: 0 },
-//           }}
-//         >
-//           <Typography
-//             variant="h5"
-//             sx={{
-//               fontWeight: "bold",
-//               textAlign: "center",
-//               padding: "6px",
-//               textDecoration: { xs: "none", md: "underline" },
-//               fontSize: { xs: "1rem", md: "1.5rem" },
-//               height: { xs: "30px", md: "40px" },
-//             }}
-//           >
-//             Select Date in A.D.
-//           </Typography>
-//           <AdView selectedDate={selectedEnglishDate} onChange={handleEngDateChange} />
-//         </Stack>
-//       </Stack>
-//     </Stack>
-//   );
-// }
-
-// export default Home;
-
 import { useState } from "react";
 import moment from "moment";
-import { useMediaQuery } from "@mui/material"; // Import useMediaQuery
-
+import { autocompleteClasses, useMediaQuery } from "@mui/material"; // Import useMediaQuery
 import NepDatePicker from "Components/NepDatePicker";
 import { ad2bsHandler, bs2adHandler } from "utils";
 
@@ -195,7 +38,7 @@ function Home() {
   return (
     <Stack
       sx={{
-        height: "100svh",
+        height: { xs: "100%", md: "100svh" },
         gap: { xs: 0, md: 2 },
       }}
     >
@@ -205,6 +48,7 @@ function Home() {
         sx={{
           height: { xs: "30px", md: "50px" },
           textAlign: "center",
+          // mb: { xs: 1, md: 0 },
         }}
       >
         <Typography
@@ -234,7 +78,7 @@ function Home() {
             border: "1px solid black",
             flex: 1,
             m: { xs: 1, md: 2 },
-            mt: { xs: 3, md: 0 },
+            mt: { xs: 1, md: 0 },
           }}
         >
           {isDesktop && ( // Only show this on desktop views

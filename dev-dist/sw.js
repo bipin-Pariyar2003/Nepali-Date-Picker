0ce2bf8388560704d5661c246e83ce3dc35b5828
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-03-13 11:38:00 +0545

    trimmed a bit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2u6kffbdqio"
+    "revision": "0.q4m9f5e18po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
